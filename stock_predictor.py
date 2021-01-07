13f4f47c4c32699d1b0bb0b95a60ebc9ac77ff92
Evan Knapke <evanknapke@gmail.com>
2021-01-07 14:10:44 -0500

    Add ability to use multiple symbols at once

--- stock_predictor.py
+++ stock_predictor.py
@@ -4,30 +4,31 @@ import json
 import requests
 
 def main():
-    #TODO: check rsi of each symbol in selected list
-    #      (alphavantage might already have something set up for this)
-    rsi_paramaters = {
-        "function": "RSI",
-        "symbol": "IBM",
-        "interval": "weekly",
-        "time_period": 10,
-        "series_type": "open",
-        "apikey": get_from_config('apikey')
-    }
+    symbols = convert_to_list( get_from_config('symbols') )
+    for symbol in symbols:
+        rsi_paramaters = {
+            "function": "RSI",
+            "symbol": symbol,
+            "interval": "weekly",
+            "time_period": 10,
+            "series_type": "open",
+            "apikey": get_from_config('apikey')
+        }
 
-    # api call
-    rsi_response = requests.get('https://www.alphavantage.co/query?', params=rsi_paramaters)
+        # api call
+        rsi_response = requests.get('https://www.alphavantage.co/query?', params=rsi_paramaters)
+        encoded_rsi = json.dumps(rsi_response.json(), sort_keys=True, indent=4) # json str
+        decoded_rsi = json.loads(encoded_rsi) # json dict
 
-    encoded_rsi = json.dumps(rsi_response.json(), sort_keys=True, indent=4) # json str
-    decoded_rsi = json.loads(encoded_rsi) # json dict
-
-    try: # try to find latest possible data
-        today = datetime.today().strftime('%Y-%m-%d')
-        print( check_rsi(decoded_rsi, today) )
-    except KeyError: # todays date may not be available yet
-        yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
-        print( check_rsi(decoded_rsi, yesterday) )
-
+        try: # try to find latest possible data
+            today = datetime.today().strftime('%Y-%m-%d')
+            print( symbol +": "+ check_rsi(decoded_rsi, today) )
+        except KeyError: # todays date may not be available yet
+            try:
+                yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+                print( symbol +": "+ check_rsi(decoded_rsi, yesterday) )
+            except KeyError:
+                print("ERROR; likely ran out of api calls")
 
 def check_rsi(json_data, date):
     rsi = float( json_data["Technical Analysis: RSI"][date]["RSI"] )
@@ -35,11 +36,13 @@ def check_rsi(json_data, date):
     over_sold_threshold = 30.0
 
     if rsi < over_sold_threshold:
-        return "buy"
+        suggestion = "buy"
     elif rsi > over_bought_threshold:
-        return "sell"
+        suggestion = "sell"
     else:
-        return "do nothing"
+        suggestion = "do nothing"
+
+    return ( "rsi:" + str(rsi) +": "+ suggestion)
 
 # retrieves items from config.ini file by key
 def get_from_config(key):
@@ -47,5 +50,9 @@ def get_from_config(key):
     config.read('config.ini')
     return config.get('auth', key)
 
+def convert_to_list(string):
+    cleaned_string = string.replace(" ", "")
+    li = list(cleaned_string.split(","))
+    return li
 
 main()

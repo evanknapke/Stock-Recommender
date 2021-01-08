edc348808dcf5519e7f75bed55c6294e7e0ef197
Evan Knapke <evanknapke@gmail.com>
2021-01-08 12:36:40 -0500

    Swap api provider to finnhub

--- stock_predictor.py
+++ stock_predictor.py
@@ -1,48 +1,44 @@
 from configparser import ConfigParser
-from datetime import datetime, timedelta
 import json
 import requests
 
 def main():
+    #retrieve user selected symbols from config.ini
     symbols = convert_to_list( get_from_config('symbols') )
+
     for symbol in symbols:
-        rsi_paramaters = {
-            "function": "RSI",
+        paramaters = {
             "symbol": symbol,
-            "interval": "weekly",
-            "time_period": 10,
-            "series_type": "open",
-            "apikey": get_from_config('apikey')
+            "resolution": "D",
+            "from": 1583098857,
+            "to": 1584308457,
+            "indicator": "rsi",
+            "timeperiod": 3,
+            "token": get_from_config('apikey')
         }
 
         # api call
-        rsi_response = requests.get('https://www.alphavantage.co/query?', params=rsi_paramaters)
+        rsi_response = requests.get('https://finnhub.io/api/v1/indicator?', params=paramaters)
         encoded_rsi = json.dumps(rsi_response.json(), sort_keys=True, indent=4) # json str
         decoded_rsi = json.loads(encoded_rsi) # json dict
 
-        try: # try to find latest possible data
-            today = datetime.today().strftime('%Y-%m-%d')
-            print( symbol +": "+ check_rsi(decoded_rsi, today) )
-        except KeyError: # todays date may not be available yet
-            try:
-                yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
-                print( symbol +": "+ check_rsi(decoded_rsi, yesterday) )
-            except KeyError:
-                print("ERROR; likely ran out of api calls")
+        suggestion = check_rsi(decoded_rsi)
+        print(symbol +": " + suggestion)
 
-def check_rsi(json_data, date):
-    rsi = float( json_data["Technical Analysis: RSI"][date]["RSI"] )
+def check_rsi(json_data):
+    rsi_list = json_data["rsi"]
     over_bought_threshold = 70.0
     over_sold_threshold = 30.0
 
-    if rsi < over_sold_threshold:
-        suggestion = "buy"
-    elif rsi > over_bought_threshold:
-        suggestion = "sell"
-    else:
-        suggestion = "do nothing"
+    for rsi in rsi_list:
+        if rsi < over_sold_threshold:
+            suggestion = " buy"
+        elif rsi > over_bought_threshold:
+            suggestion = " sell"
+        else:
+            suggestion = " do nothing"
 
-    return ( "rsi:" + str(rsi) +": "+ suggestion)
+    return suggestion
 
 # retrieves items from config.ini file by key
 def get_from_config(key):
@@ -50,6 +46,7 @@ def get_from_config(key):
     config.read('config.ini')
     return config.get('auth', key)
 
+# converts string of comma seperated values to a list
 def convert_to_list(string):
     cleaned_string = string.replace(" ", "")
     li = list(cleaned_string.split(","))
